094cad7e278bc7ce63ddea40e2ca0c072e29fc95
Gustavomullersouza <168039864+Gustavomullersouza@users.noreply.github.com>
2026-03-02 21:46:10 -0300

    Aula 01

--- src/Main.kt
+++ src/Main.kt
@@ -1,24 +1,29 @@
 fun main() {
 
+    print("Digite a operação (ex: 5 + 3): ")
+    val input = readLine()!!
 
-        print("Digite o primeiro número: ")
-        val n1 = readLine()!!.toDouble()
+    val partes = input.split(" ")
 
-        print("Digite o segundo número: ")
-        val n2 = readLine()!!.toDouble()
+    val n1 = partes[0].toInt()
+    val op = partes[1]
+    val n2 = partes[2].toInt()
 
-        print("Digite a operação (+, -, *, /): ")
-        val op = readLine()
+    // Verifica se os números estão entre 0 e 9
+    if (n1 !in 0..9 || n2 !in 0..9) {
+        println("Digite apenas números de 0 a 9.")
+        return
+    }
 
-        if (op == "+") {
-            println("Resultado: ${n1 + n2}")
-        } else if (op == "-") {
-            println("Resultado: ${n1 - n2}")
-        } else if (op == "*") {
-            println("Resultado: ${n1 * n2}")
-        } else if (op == "/") {
-            println("Resultado: ${n1 / n2}")
-        } else {
-            println("Operação inválida.")
-        }
+    if (op == "+") {
+        println("Resultado: ${n1 + n2}")
+    } else if (op == "-") {
+        println("Resultado: ${n1 - n2}")
+    } else if (op == "*") {
+        println("Resultado: ${n1 * n2}")
+    } else if (op == "/") {
+        println("Resultado: ${n1 / n2}")
+    } else {
+        println("Operação inválida.")
+    }
 }
